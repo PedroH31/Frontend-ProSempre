4e61c4a7562492118159af3b028ff9b55c0ff079
Pedro <necromancerdoidao@outlook.com>
2024-10-06 22:30:24 -0300

    adicionado header e footer
    testei a home dinâmica e organizei os arquivos em suas devidas pastas.
    
    close #1

--- DesafioProSempre/js/main.js
+++ DesafioProSempre/js/main.js
@@ -1,1 +1,11 @@
+const root = document.querySelector("#app")
 
+function renderizar(pagina) {
+    fetch(`./paginas/${pagina}/${pagina}.html`)
+        .then(response => response.text())
+        .then(data => {
+            root.innerHTML = data
+        })
+}
+
+renderizar("home") 
