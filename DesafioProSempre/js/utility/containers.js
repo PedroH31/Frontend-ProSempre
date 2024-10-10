2ec3e90ed69f2eccdb2ca14d7106f92f3fb0bdb7
Pedro Henrique <95108502+PedroH31@users.noreply.github.com>
2024-10-10 06:09:33 -0300

    criada função para replicar elementos
    finalizada página home(falta os toques finais)
    
    close #12

--- DesafioProSempre/js/utility/containers.js
+++
@@ -1,6 +0,0 @@
-// usada para replicar elementos repetitivos nas páginas
-
-import { renderizarProfissionais } from "./utils"
-import { profissionais } from "./data"
-
-renderizarProfissionais("profissionais", profissionais)
--- DesafioProSempre/js/utility/utils.js
+++ DesafioProSempre/js/utility/utils.js
@@ -1,13 +1,18 @@
 
-function renderizarProfissionais(id, data) { 
+function renderizarDivs(id, data, template) { // substitui o conteúdo da div com o id escolhido pela lista
+    // de elementos
     const elementoDaPagina = document.querySelector(`#${id}`)
-    elementoDaPagina.innerHTML = data.map((item) => { 
-        return `
-            
-        `
+    const listaDeElementos = data.map((item) => { 
+        return modelarString(template, item)
     }).join("")
+    
+    elementoDaPagina.innerHTML = listaDeElementos
 }
 
+function modelarString(template, data) { // substitui tudo que está dentro de chaves na string template
+    return template.replace(/\{(.*?)}/g, (match, key) => {
+        return data[key] || ""
+    })
+}
 
-
-export { renderizarProfissionais }
+export {renderizarDivs}
